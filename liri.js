c40024ccb0ae4b89e24ed71a59ebe034a80c03ed
Dans Bootcamp <dansbootcamp@AmbersImac.home>
2019-10-29 01:35:47 -0400

    couple last little tweaks

--- liri.js
+++ liri.js
@@ -62,7 +62,7 @@ function userCommand(userInput, userQuery) {
 userCommand(userInput, userQuery);
 
 function concertThis() {
-    console.log(`\n - - - - -\n\nSEARCHING FOR...${userQuery}'s next show...`);
+    console.log(`\n - - - - -\n\nI'm looking now..${userQuery}'s next show...`);
     // USER QUERY FOR THE BANDS IN TOWN API
     request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
         if (!error && response.statusCode === 200) {
@@ -72,21 +72,21 @@ function concertThis() {
             if (userBand.length > 0) {
                 for (i = 0; i < 1; i++) {
                     // CONSOLE LOGGING THE RESPONSE
-                    console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}\nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
+                    console.log(`\nLook who just happens to be in town \n\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}\nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
 
                     // FORMATTING THE DATE AND TIME
                     let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
                     console.log(`Date and Time: ${concertDate}\n\n- - - - -`);
                 };
             } else {
-                console.log('Band or concert not found!');
+                console.log('Unfortunately I could not find what you were looking for');
             };
         };
     });
 };
 
 function spotifyThisSong() {
-    console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
+    console.log(`\n - - - - -\n\nlet me see if I can spot ..."${userQuery}"`);
 
     // DEFAULT SONG IF USER QUERY NOT FOUND
     if (!userQuery) {
@@ -100,21 +100,21 @@ function spotifyThisSong() {
         limit: 1
     }, function (error, data) {
         if (error) {
-            return console.log('Error occurred: ' + error);
+            return console.log('Uh oh an error occurred: ' + error);
         }
         // STORING THE RESPONSE DATA IN AN ARRAY
         let spotifyArr = data.tracks.items;
 
         for (i = 0; i < spotifyArr.length; i++) {
-            console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
+            console.log(`\nI hope this hits the spot \n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
         };
     });
 }
 
 function movieThis() {
-    console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
+    console.log(`\n - - - - -\n\nI'm looking now..."${userQuery}"`);
     if (!userQuery) {
-        userQuery = "mr nobody";
+        userQuery = "jaws";
     };
     // OMDB REQUEST
     request("http://www.omdbapi.com/?t=" + userQuery + "&apikey=86fe999c", function (error, response, body) {
@@ -124,9 +124,9 @@ function movieThis() {
         if (ratingsArr.length > 2) {}
 
         if (!error && response.statusCode === 200) {
-            console.log(`\nBA DA BOP!  That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratings[1].Value}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
+            console.log(`\nAs you've requested\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratings[1].Value}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
         } else {
-            return console.log("Movie able to be found. Error:" + error)
+            return console.log("Error, Error, Error....bst, zap, malfunction. Error:" + error)
         };
     })
 };
